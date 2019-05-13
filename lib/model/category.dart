6fed44524d747796176e6cdfc2ec309417e368f0
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-05-13 18:11:47 +0300

    I've added some comments to describe the code I've used in the class.

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -15,13 +15,18 @@ class Category {
 
   String get name => _name;
 
+  //I'll use this method when I try to save the object to the local DB.
   Map<String, dynamic> toMap() {
+    //Here I'll return a map with the key names identical the column names
+    // from the remote DB for the category table.
     return {
       "event_entity_category_id": _id,
       "event_entity_category_name": _name
     };
   }
 
+  //This method will be used when the app creates a new category object using
+  // values from the DB.
   Category.fromMap(Map<String, dynamic> map) {
     _id = map['event_entity_category_id'];
     _name = map['event_entity_category_name'];
