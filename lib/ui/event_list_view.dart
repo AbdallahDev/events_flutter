90a7e5777218346d2ccfca0cdd3402fd1438b83e
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-05-14 12:43:07 +0300

    I've created a constructor with a category id.

--- lib/ui/event_list_view.dart
+++ lib/ui/event_list_view.dart
@@ -3,7 +3,10 @@
 
 import 'package:flutter/material.dart';
 
-class EventListView extends StatelessWidget{
+class EventListView extends StatelessWidget {
+  //The constructor will take the category id to view the events that belong to it.
+  EventListView({@required int categoryId});
+
   @override
   Widget build(BuildContext context) {
     return Text("data");
