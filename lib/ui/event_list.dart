6856f12d114d646399d00f32adaaea670114ed34
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-05-14 18:43:39 +0300

    I've created this class to get the event details that belong to all the entities that belong to a specific category and have events.

--- lib/ui/event_list.dart
+++ lib/ui/event_list.dart
@@ -1,26 +1,52 @@
 //This class will get the events and return them as a list.
 //I've made it separate because I don't want all the code to be in the home file.
 
+import 'package:events_flutter/model/event.dart';
 import 'package:events_flutter/util/database_helper.dart';
 import 'package:meta/meta.dart';
 
 class EventList {
-  //This method will get the events for a specific category.
-  //I made it static so it can be accessed without the need for a new object.
-  static Future<List> fillEventList({@required int categoryId}) async {
+  //This field holds an instance of the databaseHelper class that deals with the
+  // database.
+  static DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  //This function will get the event ids that belong to a specific entity from
+  // the table event entity.
+  //Then it will fill the events list with the event objects that contain the
+  // details of the event.
+  //I made it separate to make the code more concise.
+  static Future getEvents({@required int categoryId}) async {
+    //This list will contain the events objects to be returned to the home page.
+    List<Event> events = List();
     //This list will store the entity ids that I got from the function _getEntityIds.
     List entityIds = await _getEntityIds(categoryId: categoryId);
+    //Next, I'll loop over those entities to get the events that belong to each
+    // one of them.
+    entityIds.forEach((entityId) async {
+      //This list contains the event ids that belong to a specific entity.
+      List eventIds = await _databaseHelper.getEventIds(entityId: entityId);
+      //I'll make sure that the list is not empty.
+      if (eventIds.length != 0) {
+        //I'll loop over the event ids list to get the details for each event.
+        eventIds.forEach((map) async {
+          //This list will store the event details from the local DB.
+          List event = await _databaseHelper.getEvent(eventId: map['event_id']);
+          //Here I'll create for each event an object, by getting the first
+          // values in the list that represent the event map, then I'll add it
+          // to the events list.
+          events.add(Event.fromMap(event.first));
+        });
+      }
+    });
 
-    //I'll loop over the entity ids to get the events that belong to each one.
-    entityIds.forEach((id) {});
+    return events;
   }
 
   //This function will get the entity ids that belong to a specific category.
   //And I've made this function separate to make the code more organized.
   static Future<List<int>> _getEntityIds({@required int categoryId}) async {
-    DatabaseHelper databaseHelper = DatabaseHelper();
     //First I should get the entity ids that belong to the specified category.
-    List result = await databaseHelper.getEntityIds(categoryId: categoryId);
+    List result = await _databaseHelper.getEntityIds(categoryId: categoryId);
     //This list will hold the entity ids.
     List<int> entityIds = List();
     //I'll loop over the result list to store the ids as integers in the entityIds list.
