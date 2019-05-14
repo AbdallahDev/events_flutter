3f86f878589ee483669362a32adb4342dc617806
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-05-14 12:17:32 +0300

    I've made the event ListView class as a widget to be able to inflate it in the home screen.

--- lib/ui/event_list.dart
+++ lib/ui/event_list.dart
@@ -1,2 +1,12 @@
 //This class will create the event ListView.
 //I've made it separate because I don't want all the code to be in the home file.
+
+import 'package:flutter/material.dart';
+
+class EventListView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
