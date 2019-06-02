fcbe50b765d9af542f01956598aa79e253db8362
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-06-02 12:59:28 +0300

    I've created a stateFul widget in the file to be able to run the firebase messaging code.

---
+++ lib/ui/notification_receiver.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotificationReceiver extends StatefulWidget {
+  @override
+  _NotificationReceiverState createState() => _NotificationReceiverState();
+}
+
+class _NotificationReceiverState extends State<NotificationReceiver> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
