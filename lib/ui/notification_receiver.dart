e9bfdc4a58f5f0d03738a44653781114e2001c9a
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-06-02 13:02:02 +0300

    I've imported the messaging and the local notification libraries.

--- lib/ui/notification_receiver.dart
+++ lib/ui/notification_receiver.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationReceiver extends StatefulWidget {
   @override
