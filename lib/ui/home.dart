ac5e679906d1febb9b1ecc47b83ed9a7da7f7805
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-07-25 14:21:26 +0300

    I've changed the _deviceIdentifier type to be String.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -38,7 +38,7 @@ class _HomeState extends State<Home> {
 
   //This field stores the device identifier, I'll use it to avoid tokens
   // duplication in the DB.
-  var _deviceIdentifier;
+  String _deviceIdentifier;
 
   //I need the initState function to run some of the code just at the first time
   // the app runs.
