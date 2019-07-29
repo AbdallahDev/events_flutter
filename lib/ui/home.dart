bf7e50afafdfde6ff3c5bfa1a5a8ac2dc81ba9ee
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-07-29 10:52:36 +0300

    I've updated the apiURL to point to the working events web app.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -18,7 +18,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //This is the API base URL.
-  var apiURL = "http://10.152.168.198/events/mobile/apis/";
+  var apiURL = "http://193.188.88.148/events/mobile/apis/";
 
   //This list to store the category objects.
   List<Category> _categories;
