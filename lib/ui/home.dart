081be9fb2370aeecd8ea8df2fe5aae4b5416e845
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-09-04 14:04:52 +0300

    I've reverted to the test server IP.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -19,7 +19,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //This is the API base URL.
-  var apiURL = "http://10.152.232.199/apps/myapps/events/mobile/apis/";
+  var apiURL = "http://193.188.88.148/apps/myapps/events/mobile/apis/";
 
   //This list to store the category objects.
   List<Category> _categories;
