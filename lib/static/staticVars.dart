5b85808ee6a966d9cc880fcc3f51df6325122641
abdallahdev <abdallah.dev85@gmail.com>
2019-09-25 15:32:31 +0300

    I've created two variables for the API URL to make it easy for me when switching between the URL of the working web system and the testing one.

--- lib/static/staticVars.dart
+++ lib/static/staticVars.dart
@@ -3,5 +3,10 @@
 class StaticVars{
   //I've made the API URL variable as static because I want to access it
   // globally and I don't want to change it every time in the home file.
-  static final apiUrl = "http://193.188.88.148/events/mobile/apis/";
+  static final apiUrl = url;
+
+  //I've created these two variables to make it easy for me to switch between
+  // the API URL of the working web system and the testing one.
+ static var url = "http://193.188.88.148/events/mobile/apis/";
+ static var url0 = "http://193.188.88.148/apps/myapps/events/mobile/apis/";
 }
