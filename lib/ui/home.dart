eb8e2668990edcf062d7d6b097b3a042ca99e04a
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-10-10 10:57:00 +0300

    I've added a sub Row in the top Row to combine the all days checkbox and the specific day calendar.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -294,6 +294,7 @@ class _HomeState extends State<Home> {
                     ),
                   ],
                 ),
+                Row(children: <Widget>[],),
               ],
             ),
             Center(
