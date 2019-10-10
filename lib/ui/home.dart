256bbb322d95ead963c8d61a7d77ba88bdcb215c
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-10-10 10:25:55 +0300

    I've made the text Direction of the Row RTL to make the dropdown button aligned to the most right of it.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -229,6 +229,7 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             Row(
+              textDirection: _rtlTextDirection,
               children: <Widget>[
                 Column(
                   children: <Widget>[],
