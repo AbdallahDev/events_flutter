d1979f7ff14943daf33d5c08fcfddd108633d9ce
abdallahdev <abdallah.dev85@gmail.com>
2019-12-03 13:30:08 +0200

    I've modified the URL value to the right one.

--- lib/static/staticVars.dart
+++ lib/static/staticVars.dart
@@ -3,7 +3,7 @@
 class StaticVars {
   //I've made the API URL variable as static because I want to access it
   // globally and I don't want to change it every time in the home file.
-  static final apiUrl = url0;
+  static final apiUrl = url;
 
   //I've created these two variables to make it easy for me to switch between
   // the API URL of the working web system and the testing one.
